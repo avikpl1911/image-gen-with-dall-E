9459ae3914cdf570ace0c69d903c11539299018b
lolxd <65647597+pnxmyth@users.noreply.github.com>
2023-02-15 18:38:35 +0530

    alive

---
+++ alive/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+
+
+const app = express();
+
+app.use(cors());
+
+setInterval(async()=>{
+  
+    try{await axios.get("https://dalle-post.onrender.com/")}catch(err){}
+
+},5000);
+
+//for first call
+
+async()=>{
+    try{await axios.get("https://dalle-post.onrender.com/")}catch(err){}
+    
+}
+
+
+
+
+
+
+const startServer = async ()=>{
+    
+    app.listen(process.env.PORT || 8800,(err)=>{
+      if (err){
+        console.log(err);
+      }else{
+        console.log("server is running on port http://localhost:8800")
+      }
+    });
+}
+
+startServer();
+
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dallie', {
+        const response = await fetch('https://image-gen-with-dall-e.vercel.app/api/v1/dallie', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dalle-post.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
